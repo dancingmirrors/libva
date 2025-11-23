157d7e630428285d2b5c772bec5c7c48a8543675
dancingmirrors <dancingmirrors@icloud.com>
2025-11-22 23:04:03 -0700

    Disable probing for the iHD driver on i915.

--- va/drm/va_drm_utils.c
+++ va/drm/va_drm_utils.c
@@ -60,7 +60,7 @@ VA_DRM_GetDriverNames(VADriverContextP ctx, char **drivers, unsigned *num_driver
         const char * const va_driver[MAX_NAMES];
     } map[] = {
         { "xe",         { "iHD"              } },
-        { "i915",       { "iHD", "i965"      } }, // Intel Media and OTC GenX
+        { "i915",       { "i965"             } }, // Intel Media and OTC GenX
         { "pvrsrvkm",   { "pvr"              } }, // Intel UMG PVR
         { "radeon",     { "r600", "radeonsi" } }, // Mesa Gallium
         { "amdgpu",     { "radeonsi"         } }, // Mesa Gallium
